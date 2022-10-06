cd163d8b579df00667efc5b22ce418fb8d983dd6
vikrantbhawanisaini2022 <vikrant@fictivebox.com>
2022-10-06 11:38:46 +0530

    Widget call

--- lib/View/Home/Pages/home_page.dart
+++ lib/View/Home/Pages/home_page.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lite_rolling_switch/lite_rolling_switch.dart';
@@ -11,8 +9,6 @@ import '../../../Consts/text_const.dart';
 import '../../../consts/image_const.dart';
 import '../../../consts/themescolor.dart';
 import '../../../route_app/page_navigeter_name_route.dart';
-
-
 class Home_page_Screen extends StatefulWidget {
   @override
   State<Home_page_Screen> createState() => _Home_page_ScreenState();
@@ -20,29 +16,23 @@ class Home_page_Screen extends StatefulWidget {
 
 class _Home_page_ScreenState extends State<Home_page_Screen> {
   var ht, wt;
-
   late Online_Ofline_View_Model _online_ofline_view_model;
-
   late Booking_Request_View_Model booking_request_view_model;
 
   Future<void> _refresh(bool reload, BuildContext context){
      booking_request_view_model= Provider.of<Booking_Request_View_Model>(context,listen: false);
     booking_request_view_model.getbookingApiCall(reload);
     return Future.delayed(const Duration(seconds: 0));
-
-
   }
 
   void initState(){
     super.initState();
 
   }
-
   @override
   Widget build(BuildContext context) {
     Booking_Request_View_Model booking_request_view_model =
         context.watch<Booking_Request_View_Model>();
-
     wt = MediaQuery.of(context).size.width;
     ht = MediaQuery.of(context).size.height;
     return SafeArea(
@@ -59,56 +49,12 @@ class _Home_page_ScreenState extends State<Home_page_Screen> {
       ),
     );
   }
-
   _listDesign(Booking_Request_View_Model booking_request_view_model,
       BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: const EdgeInsets.only(left: 16, right: 16, top: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, RouteName.Profile_Screen);
-                },
-                child: const CircleAvatar(
-                  radius: 20,
-                  backgroundImage: NetworkImage(
-                      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80'),
-                  backgroundColor: Colors.transparent,
-                ),
-              ),
-              SizedBox(
-                height: 31,
-                width: 120,
-                child: LiteRollingSwitch(
-                  textOn: "Online",
-                  value: true,
-                  textOff: "Ofline",
-                  colorOn: btn,
-                  colorOff: p1Color,
-                  textSize: 16,
-                  onChanged: (val) {
-                    _online_ofline_view_model =
-                        Provider.of<Online_Ofline_View_Model>(context,
-                            listen: false);
-                    _online_ofline_view_model.getonlineofline(
-                        userStatus: val ? "1" : "0");
-                  },
-                ),
-              ),
-              InkWell(
-                onTap: () {
-                  Navigator.pushNamed(context, RouteName.Notifications_screen);
-                },
-                child: const Icon(Icons.notifications),
-              )
-            ],
-          ),
-        ),
+        appBar(),
         const SizedBox(
           height: 32,
         ),
@@ -130,7 +76,6 @@ class _Home_page_ScreenState extends State<Home_page_Screen> {
             )
           ],
         ),
-
         Padding(
           padding: const EdgeInsets.only(left: 16, right: 16, top: 18),
           child: Text(
@@ -143,6 +88,52 @@ class _Home_page_ScreenState extends State<Home_page_Screen> {
             child: SingleChildScrollView(
                 child: BookingListTitl(booking_request_view_model)))
       ],
+    );
+  }
+  Widget appBar(){
+    return  Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, RouteName.Profile_Screen);
+            },
+            child: const CircleAvatar(
+              radius: 20,
+              backgroundImage: NetworkImage(
+                  'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlciUyMHByb2ZpbGV8ZW58MHx8MHx8&w=1000&q=80'),
+              backgroundColor: Colors.transparent,
+            ),
+          ),
+          SizedBox(
+            height: 31,
+            width: 120,
+            child: LiteRollingSwitch(
+              textOn: "Online",
+              value: true,
+              textOff: "Ofline",
+              colorOn: btn,
+              colorOff: p1Color,
+              textSize: 16,
+              onChanged: (val) {
+                _online_ofline_view_model =
+                    Provider.of<Online_Ofline_View_Model>(context,
+                        listen: false);
+                _online_ofline_view_model.getonlineofline(
+                    userStatus: val ? "1" : "0");
+              },
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, RouteName.Notifications_screen);
+            },
+            child: const Icon(Icons.notifications),
+          )
+        ],
+      ),
     );
   }
 }
